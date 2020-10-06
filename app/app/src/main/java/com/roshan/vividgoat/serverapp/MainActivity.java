19f2a40b3cf61101593e52eae0ff5ce84112b26f
S.K.Roshan <63838060+dev-Roshan-lab@users.noreply.github.com>
2020-10-06 21:39:12 +0530

    Add files via upload

---
+++ app/app/src/main/java/com/roshan/vividgoat/serverapp/MainActivity.java
@@ -0,0 +1,467 @@
+package com.roshan.vividgoat.serverapp;
+
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.view.View.*;
+import android.widget.*;
+import android.content.*;
+import android.graphics.*;
+import android.media.*;
+import android.net.*;
+import android.text.*;
+import android.util.*;
+import android.webkit.*;
+import android.animation.*;
+import android.view.animation.*;
+import java.util.*;
+import java.text.*;
+import android.support.v7.app.AppCompatActivity;
+import java.util.HashMap;
+import java.util.ArrayList;
+import android.widget.LinearLayout;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Button;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.ChildEventListener;
+import java.util.Calendar;
+import java.text.SimpleDateFormat;
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.os.Build;
+import android.support.v4.content.FileProvider;
+import java.io.File;
+import android.content.ClipData;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import android.view.View;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.app.ActivityCompat;
+import android.Manifest;
+import android.content.pm.PackageManager;
+
+public class MainActivity extends AppCompatActivity {
+	
+	public final int REQ_CD_CAM = 101;
+	public final int REQ_CD_FP = 102;
+	private FirebaseDatabase _firebase = FirebaseDatabase.getInstance();
+	private FirebaseStorage _firebase_storage = FirebaseStorage.getInstance();
+	
+	private String pos = "";
+	private String time = "";
+	private double no = 0;
+	private HashMap<String, Object> mapvar = new HashMap<>();
+	
+	private ArrayList<HashMap<String, Object>> map = new ArrayList<>();
+	private ArrayList<String> str = new ArrayList<>();
+	private ArrayList<String> logs = new ArrayList<>();
+	
+	private LinearLayout linear4;
+	private LinearLayout linear5;
+	private ImageView imageview1;
+	private LinearLayout linear6;
+	private ProgressBar progressbar1;
+	private TextView textview6;
+	private TextView textview5;
+	private Button button1;
+	private Button button2;
+	
+	private DatabaseReference db = _firebase.getReference("/");
+	private ChildEventListener _db_child_listener;
+	private Calendar tm = Calendar.getInstance();
+	private Calendar tm2 = Calendar.getInstance();
+	private SharedPreferences data;
+	private Intent cam = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+	private File _file_cam;
+	private Intent fp = new Intent(Intent.ACTION_GET_CONTENT);
+	private RequestNetwork api;
+	private RequestNetwork.RequestListener _api_request_listener;
+	private StorageReference fbs = _firebase_storage.getReference("pictures");
+	private OnSuccessListener<UploadTask.TaskSnapshot> _fbs_upload_success_listener;
+	private OnSuccessListener<FileDownloadTask.TaskSnapshot> _fbs_download_success_listener;
+	private OnSuccessListener _fbs_delete_success_listener;
+	private OnProgressListener _fbs_upload_progress_listener;
+	private OnProgressListener _fbs_download_progress_listener;
+	private OnFailureListener _fbs_failure_listener;
+	@Override
+	protected void onCreate(Bundle _savedInstanceState) {
+		super.onCreate(_savedInstanceState);
+		setContentView(R.layout.main);
+		initialize(_savedInstanceState);
+		if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED
+		|| ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED
+		|| ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+			ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1000);
+		}
+		else {
+			initializeLogic();
+		}
+	}
+	@Override
+	public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+		super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+		if (requestCode == 1000) {
+			initializeLogic();
+		}
+	}
+	
+	private void initialize(Bundle _savedInstanceState) {
+		
+		linear4 = (LinearLayout) findViewById(R.id.linear4);
+		linear5 = (LinearLayout) findViewById(R.id.linear5);
+		imageview1 = (ImageView) findViewById(R.id.imageview1);
+		linear6 = (LinearLayout) findViewById(R.id.linear6);
+		progressbar1 = (ProgressBar) findViewById(R.id.progressbar1);
+		textview6 = (TextView) findViewById(R.id.textview6);
+		textview5 = (TextView) findViewById(R.id.textview5);
+		button1 = (Button) findViewById(R.id.button1);
+		button2 = (Button) findViewById(R.id.button2);
+		data = getSharedPreferences("data", Activity.MODE_PRIVATE);
+		_file_cam = FileUtil.createNewPictureFile(getApplicationContext());
+		Uri _uri_cam = null;
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+			_uri_cam= FileProvider.getUriForFile(getApplicationContext(), getApplicationContext().getPackageName() + ".provider", _file_cam);
+		}
+		else {
+			_uri_cam = Uri.fromFile(_file_cam);
+		}
+		cam.putExtra(MediaStore.EXTRA_OUTPUT, _uri_cam);
+		cam.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+		fp.setType("image/*");
+		fp.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+		api = new RequestNetwork(this);
+		
+		button1.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				startActivityForResult(fp, REQ_CD_FP);
+			}
+		});
+		
+		button2.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				startActivityForResult(cam, REQ_CD_CAM);
+			}
+		});
+		
+		_db_child_listener = new ChildEventListener() {
+			@Override
+			public void onChildAdded(DataSnapshot _param1, String _param2) {
+				GenericTypeIndicator<HashMap<String, Object>> _ind = new GenericTypeIndicator<HashMap<String, Object>>() {};
+				final String _childKey = _param1.getKey();
+				final HashMap<String, Object> _childValue = _param1.getValue(_ind);
+				db.addListenerForSingleValueEvent(new ValueEventListener() {
+					@Override
+					public void onDataChange(DataSnapshot _dataSnapshot) {
+						map = new ArrayList<>();
+						try {
+							GenericTypeIndicator<HashMap<String, Object>> _ind = new GenericTypeIndicator<HashMap<String, Object>>() {};
+							for (DataSnapshot _data : _dataSnapshot.getChildren()) {
+								HashMap<String, Object> _map = _data.getValue(_ind);
+								map.add(_map);
+							}
+						}
+						catch (Exception _e) {
+							_e.printStackTrace();
+						}
+						str.clear();
+						for (DataSnapshot dshot:
+						_dataSnapshot.getChildren()) {
+							
+							str.add(dshot.getKey());
+							
+						}
+					}
+					@Override
+					public void onCancelled(DatabaseError _databaseError) {
+					}
+				});
+			}
+			
+			@Override
+			public void onChildChanged(DataSnapshot _param1, String _param2) {
+				GenericTypeIndicator<HashMap<String, Object>> _ind = new GenericTypeIndicator<HashMap<String, Object>>() {};
+				final String _childKey = _param1.getKey();
+				final HashMap<String, Object> _childValue = _param1.getValue(_ind);
+				db.addListenerForSingleValueEvent(new ValueEventListener() {
+					@Override
+					public void onDataChange(DataSnapshot _dataSnapshot) {
+						map = new ArrayList<>();
+						try {
+							GenericTypeIndicator<HashMap<String, Object>> _ind = new GenericTypeIndicator<HashMap<String, Object>>() {};
+							for (DataSnapshot _data : _dataSnapshot.getChildren()) {
+								HashMap<String, Object> _map = _data.getValue(_ind);
+								map.add(_map);
+							}
+						}
+						catch (Exception _e) {
+							_e.printStackTrace();
+						}
+						str.clear();
+						for (DataSnapshot dshot:
+						_dataSnapshot.getChildren()) {
+							
+							str.add(dshot.getKey());
+							
+						}
+						if (map.get((int)0).get("message").toString().equals("yes")) {
+							
+							
+							_Round(20, 20, 20, 20, "#F44336", button1);
+							_setElevation(button1, 20);
+							
+							
+						}
+						else {
+							
+							
+							_Round(20, 20, 20, 20, "#2E7D32", button1);
+							_setElevation(button1, 20);
+							
+							
+						}
+					}
+					@Override
+					public void onCancelled(DatabaseError _databaseError) {
+					}
+				});
+			}
+			
+			@Override
+			public void onChildMoved(DataSnapshot _param1, String _param2) {
+				
+			}
+			
+			@Override
+			public void onChildRemoved(DataSnapshot _param1) {
+				GenericTypeIndicator<HashMap<String, Object>> _ind = new GenericTypeIndicator<HashMap<String, Object>>() {};
+				final String _childKey = _param1.getKey();
+				final HashMap<String, Object> _childValue = _param1.getValue(_ind);
+				
+			}
+			
+			@Override
+			public void onCancelled(DatabaseError _param1) {
+				final int _errorCode = _param1.getCode();
+				final String _errorMessage = _param1.getMessage();
+				
+			}
+		};
+		db.addChildEventListener(_db_child_listener);
+		
+		_api_request_listener = new RequestNetwork.RequestListener() {
+			@Override
+			public void onResponse(String _param1, String _param2) {
+				final String _tag = _param1;
+				final String _response = _param2;
+				if (_response.equals("Pneumonia")) {
+					textview6.setText("Suffering from Pneumonia");
+				}
+				else {
+					textview6.setText("You're Healthy");
+				}
+				db.child("1234").removeValue();
+			}
+			
+			@Override
+			public void onErrorResponse(String _param1, String _param2) {
+				final String _tag = _param1;
+				final String _message = _param2;
+				
+			}
+		};
+		
+		_fbs_upload_progress_listener = new OnProgressListener<UploadTask.TaskSnapshot>() {
+			@Override
+			public void onProgress(UploadTask.TaskSnapshot _param1) {
+				double _progressValue = (100.0 * _param1.getBytesTransferred()) / _param1.getTotalByteCount();
+				progressbar1.setProgress((int)_progressValue);
+			}
+		};
+		
+		_fbs_download_progress_listener = new OnProgressListener<FileDownloadTask.TaskSnapshot>() {
+			@Override
+			public void onProgress(FileDownloadTask.TaskSnapshot _param1) {
+				double _progressValue = (100.0 * _param1.getBytesTransferred()) / _param1.getTotalByteCount();
+				
+			}
+		};
+		
+		_fbs_upload_success_listener = new OnSuccessListener<UploadTask.TaskSnapshot>() {
+			@Override
+			public void onSuccess(UploadTask.TaskSnapshot _param1) {
+				final String _downloadUrl = _param1.getDownloadUrl().toString();
+				Util.showMessage(getApplicationContext(), "Uploaded to Cloud");
+				mapvar = new HashMap<>();
+				mapvar.put("url", _downloadUrl);
+				db.child("1234").updateChildren(mapvar);
+				mapvar.clear();
+				api.startRequestNetwork(RequestNetworkController.GET, "http://drding4tuberculosis.herokuapp.com/home/".concat("1234"), "lol", _api_request_listener);
+			}
+		};
+		
+		_fbs_download_success_listener = new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+			@Override
+			public void onSuccess(FileDownloadTask.TaskSnapshot _param1) {
+				final long _totalByteCount = _param1.getTotalByteCount();
+				
+			}
+		};
+		
+		_fbs_delete_success_listener = new OnSuccessListener() {
+			@Override
+			public void onSuccess(Object _param1) {
+				
+			}
+		};
+		
+		_fbs_failure_listener = new OnFailureListener() {
+			@Override
+			public void onFailure(Exception _param1) {
+				final String _message = _param1.getMessage();
+				
+			}
+		};
+	}
+	private void initializeLogic() {
+	}
+	
+	@Override
+	protected void onActivityResult(int _requestCode, int _resultCode, Intent _data) {
+		super.onActivityResult(_requestCode, _resultCode, _data);
+		
+		switch (_requestCode) {
+			case REQ_CD_CAM:
+			if (_resultCode == Activity.RESULT_OK) {
+				 String _filePath = _file_cam.getAbsolutePath();
+				
+				imageview1.setImageBitmap(FileUtil.decodeSampleBitmapFromPath(_filePath, 1024, 1024));
+				fbs.child(_filePath).putFile(Uri.fromFile(new File(_filePath))).addOnSuccessListener(_fbs_upload_success_listener).addOnFailureListener(_fbs_failure_listener).addOnProgressListener(_fbs_upload_progress_listener);
+			}
+			else {
+				
+			}
+			break;
+			
+			case REQ_CD_FP:
+			if (_resultCode == Activity.RESULT_OK) {
+				ArrayList<String> _filePath = new ArrayList<>();
+				if (_data != null) {
+					if (_data.getClipData() != null) {
+						for (int _index = 0; _index < _data.getClipData().getItemCount(); _index++) {
+							ClipData.Item _item = _data.getClipData().getItemAt(_index);
+							_filePath.add(FileUtil.convertUriToFilePath(getApplicationContext(), _item.getUri()));
+						}
+					}
+					else {
+						_filePath.add(FileUtil.convertUriToFilePath(getApplicationContext(), _data.getData()));
+					}
+				}
+				imageview1.setImageBitmap(FileUtil.decodeSampleBitmapFromPath(_filePath.get((int)(0)), 1024, 1024));
+				fbs.child(_filePath.get((int)(0))).putFile(Uri.fromFile(new File(_filePath.get((int)(0))))).addOnSuccessListener(_fbs_upload_success_listener).addOnFailureListener(_fbs_failure_listener).addOnProgressListener(_fbs_upload_progress_listener);
+			}
+			else {
+				
+			}
+			break;
+			default:
+			break;
+		}
+	}
+	
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		no = 0;
+		for(int _repeat10 = 0; _repeat10 < (int)(logs.size()); _repeat10++) {
+			data.edit().putString(String.valueOf((long)(no)), logs.get((int)(no))).commit();
+			no++;
+		}
+		data.edit().putString("tot", String.valueOf((long)(logs.size()))).commit();
+	}
+	private void _Round (final double _one, final double _two, final double _three, final double _four, final String _color, final View _view) {
+		Double left_top = _one;
+		Double right_top = _two;
+		Double left_bottom = _three;
+		Double right_bottom = _four;
+		android.graphics.drawable.GradientDrawable s = new android.graphics.drawable.GradientDrawable();
+		s.setShape(android.graphics.drawable.GradientDrawable.RECTANGLE);
+		s.setCornerRadii(new float[] {left_top.floatValue(),left_top.floatValue(), right_top.floatValue(),right_top.floatValue(), left_bottom.floatValue(),left_bottom.floatValue(), right_bottom.floatValue(),right_bottom.floatValue()});
+		s.setColor(Color.parseColor(_color));
+		_view.setBackground(s);
+	}
+	
+	
+	private void _setElevation (final View _view, final double _value) {
+		_view.setElevation((float)_value);
+	}
+	
+	
+	@Deprecated
+	public void showMessage(String _s) {
+		Toast.makeText(getApplicationContext(), _s, Toast.LENGTH_SHORT).show();
+	}
+	
+	@Deprecated
+	public int getLocationX(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[0];
+	}
+	
+	@Deprecated
+	public int getLocationY(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[1];
+	}
+	
+	@Deprecated
+	public int getRandom(int _min, int _max) {
+		Random random = new Random();
+		return random.nextInt(_max - _min + 1) + _min;
+	}
+	
+	@Deprecated
+	public ArrayList<Double> getCheckedItemPositionsToArray(ListView _list) {
+		ArrayList<Double> _result = new ArrayList<Double>();
+		SparseBooleanArray _arr = _list.getCheckedItemPositions();
+		for (int _iIdx = 0; _iIdx < _arr.size(); _iIdx++) {
+			if (_arr.valueAt(_iIdx))
+			_result.add((double)_arr.keyAt(_iIdx));
+		}
+		return _result;
+	}
+	
+	@Deprecated
+	public float getDip(int _input){
+		return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, _input, getResources().getDisplayMetrics());
+	}
+	
+	@Deprecated
+	public int getDisplayWidthPixels(){
+		return getResources().getDisplayMetrics().widthPixels;
+	}
+	
+	@Deprecated
+	public int getDisplayHeightPixels(){
+		return getResources().getDisplayMetrics().heightPixels;
+	}
+	
+}
